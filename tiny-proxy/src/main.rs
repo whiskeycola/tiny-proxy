6ee20409e41ad2b96d63ca7327d5cd233e101383
geniusmerely <dima.tlens@gmail.com>
2024-10-04 23:48:43 +0300

    add error handler

--- tiny-proxy/src/main.rs
+++ tiny-proxy/src/main.rs
@@ -3,14 +3,17 @@ use tiny_proxy_core::Proxy;
 #[tokio::main]
 async fn main() {
     let proxy = Proxy::default().add_user("dima", "qwe123");
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+    println!("{}", listener.local_addr().unwrap());
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
 
         let proxy = proxy.clone();
-        tokio::spawn(async move { proxy.run(socket).await });
+        tokio::spawn(async move {
+            if let Err(e) = proxy.run(socket).await {
+                println!("{e}");
+            }
+        });
     }
 }
